ffc96a772e5c392816768dbd015291fbe47aefdd
Faith Samuel <faithsamuel365@gmail.com>
2026-09-10 23:32:16 +0100

    Deleted some vite files

--- src/main.js
+++ src/main.js
@@ -1,60 +1,240 @@
-import './style.css'
-import heroImg from './assets/hero.png'
-import javascriptLogo from './assets/javascript.svg'
-import viteLogo from './assets/vite.svg'
-import { setupCounter } from './counter.js'
+const API_KEY = "";
 
-document.querySelector('#app').innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${javascriptLogo}" class="framework" alt="JavaScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.js</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
+  const searchForm = document.querySelector(".search-form");
+  const searchInput = document.querySelector('input[name="movie"]');
+  const searchButton = document.querySelector(".search-button");
+  const movieGrid = document.querySelector(".movie-grid");
+  const movieDetails = document.querySelector("#movie-details");
+const detailsContent = document.querySelector(".details-content");
+const backButton = document.querySelector(".back-button");
+const resultsHeading = document.querySelector(".results-heading");
 
-<div class="ticks"></div>
+// Poster HelperFnc
+function getPosterUrl(poster) {
+  return poster !== "N/A" ? poster : "./assets/images/placeholder-poster.png"
+}
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src="${viteLogo}" alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-          <img class="button-icon" src="${javascriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
+// N/A HelperFnc
+function formatValue(value) {
+  return value && value !== "N/A" ? value : "Not available";
+}
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`
+// Display Movie details
 
-setupCounter(document.querySelector('#counter'))
+function displayMovieDetails(movie) {
+  const poster = getPosterUrl(movie.Poster);
+
+  detailsContent.innerHTML = `
+    <div class="details-poster">
+      <img
+        src="${poster}"
+        alt="${movie.Title} movie poster"
+      />
+    </div>
+
+    <div class="details-info">
+      <h2>${movie.Title}</h2>
+
+      <div class="details-meta">
+        <span>${movie.Year}</span>
+        <span>${formatValue(movie.Runtime)}</span>
+        <span>⭐ ${formatValue(movie.imdbRating)}</span>
+      </div>
+
+      <p class="details-genre">${formatValue(movie.Genre)}</p>
+
+      <p class="details-plot">${formatValue(movie.Plot)}</p>
+
+      <p><strong>Director:</strong> ${formatValue(movie.Director)}</p>
+
+      <p><strong>Actors:</strong> ${formatValue(movie.Actors)}</p>
+    </div>
+  `;
+}
+
+  async function getMovieDetails(imdbID) {
+    detailsContent.innerHTML = `
+    <p class="search-message">Loading movie details...</p>
+  `;
+
+  try {
+     const API_URL = `https://www.omdbapi.com/?apikey=${API_KEY}&i=${imdbID}`;
+
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+    if (data.Response === "True") {
+      displayMovieDetails(data);
+    } else {
+      detailsContent.innerHTML = `
+        <p class="search-message">${data.Error}</p>
+      `;
+    }
+    
+  } catch (error) {
+   console.error(error);
+
+    detailsContent.innerHTML = `
+      <p class="search-message">
+        Something went wrong. Please try again.
+      </p>
+    `;
+  }
+}
+
+
+
+  getMovieDetails("tt0372784");
+
+
+  // Displays Movies
+  function displayMovies(movies){
+
+    movieGrid.innerHTML = "";
+    movies.map((movie) => {
+        const poster =
+      getPosterUrl(movie.Poster);
+
+        const movieCard = `
+          <div class="movie-card" data-imdb-id="${movie.imdbID}" tabindex="0" role="button" aria-label="View details for ${movie.Title}">
+         <img
+          src="${poster}"
+          alt="${movie.Title} movie poster"
+        />
+
+        <div class="movie-info">
+          <h3>${movie.Title}</h3>
+
+          <div class="movie-meta">
+            <span>${movie.Year}</span>
+            <span>${movie.Type}</span>
+          </div>
+        </div>
+      </div>
+        `;
+
+        movieGrid.innerHTML += movieCard
+    });
+  }
+
+
+  async function searchMovies(movie) {
+    movieGrid.innerHTML = `
+  <p class="search-message">Searching for movies...</p>
+`;
+   
+  searchButton.disabled = true;
+  searchButton.textContent = "Searching...";
+
+
+   try {
+     const API_URL = `https://www.omdbapi.com/?apikey=${API_KEY}&s=${movie}`;
+
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+    if(data.Response === "True") {
+        resultsHeading.textContent = `Results for "${movie}" (${data.totalResults})`;
+
+        displayMovies(data.Search);
+    } else {
+      resultsHeading.textContent = "Search results";
+
+        movieGrid.innerHTML = `
+  <p class="search-message">${data.Error}</p>
+`;
+    }
+   } catch (error) {
+    console.error(error);
+
+    movieGrid.innerHTML = `
+      <p class="search-message">
+        Something went wrong. Please try again.
+      </p>
+    `;
+   } finally {
+    searchButton.disabled = false;
+    searchButton.textContent = "Search";
+   }
+  }
+
+  
+
+  searchForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+  
+    const movie = searchInput.value.trim();
+
+    if (!movie) {
+      searchInput.value = "";
+      resultsHeading.textContent = "Search results";
+
+        movieGrid.innerHTML = ` <div class="empty-state">
+      <div class="empty-state-icon">🎬</div>
+      <h3>What would you like to watch?</h3>
+      <p>Enter a movie title above to start searching.</p>
+    </div>`;
+
+        return;
+    }
+    searchMovies(movie);
+  })
+
+  movieGrid.addEventListener("click", (event)=> {
+   
+    const movieCard = event.target.closest(".movie-card");
+
+    if(!movieCard) return;
+
+    const imdbID = movieCard.dataset.imdbId;
+
+    document.querySelector(".results-section").style.display = "none";
+movieDetails.style.display = "block";
+
+     window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+
+// Key press
+
+
+
+    getMovieDetails(imdbID);
+  })
+
+
+  //Keypress
+  
+   movieGrid.addEventListener("keydown", (event)=> {
+     if (event.key !== "Enter") return;
+    const movieCard = event.target.closest(".movie-card");
+
+    if(!movieCard) return;
+
+    const imdbID = movieCard.dataset.imdbId;
+
+    document.querySelector(".results-section").style.display = "none";
+movieDetails.style.display = "block";
+
+     window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+
+// Key press
+
+
+
+    getMovieDetails(imdbID);
+  })
+
+
+  // Back btn handler 
+
+  backButton.addEventListener("click", () => {
+  movieDetails.style.display = "none";
+  document.querySelector(".results-section").style.display = "block";
+});
+
+  
